2bafe74252707f1404142efb27b5cfe3b3643045
agarcluq <agarcluq@everis.com>
2021-04-28 13:19:48 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,47 +1,52 @@
 import React, {  useState, useEffect, useReducer } from "react";
 import "./style.css";
-
+/**
+ * Hook tipo reducer para aumentar o reducir
+ */
 const initialState = 0;
 const reducer = (state, action) => {
   switch (action) {
     case 'increment': return state + 1;
     case 'decrement': return state - 1;
-    case 'reset': return 0;
     default: throw new Error('Unexpected action');
   }
 };
-
+/**
+ * Componente Lista de la compra
+ */
 export default function App() {
-
+  // Lista de tareas/artículos
   const [todos,setTodo]=useState([])
+  // Contador de tareas/artículos
   const [totalTodo,dispatch]=useReducer(reducer, initialState);
+   // Posición de los colores
   const [colorPosition,setColorPosition]=useState(0);
 
+  // Añadir artículo
   const addTodo=(todo)=>{
     setTodo([...todos,todo]);
-    // setTotalTodo(totalTodo+1)
     dispatch('increment')
     document.getElementById("input-todo").value='';
   }
 
+  // Eliminar artículo
   const deleteTodo=(todo)=>{
     var index=todos.indexOf(todo);
     todos.splice(index,1);
     setTodo([...todos])
-    // setTotalTodo(totalTodo-1)
     dispatch('decrement')
   }
   
-  // Cuando cambia el total
+  // Efecto que depende del total de artículos
   useEffect(()=>{
-    const colores =['red','green','grey','purple']
+    const colors =['red','green','grey','purple']
     let total= document.getElementById('total');
     if(colorPosition<4){
     setColorPosition(colorPosition+1)
     }else{
       setColorPosition(0)
     }
-      total.style.color=colores[colorPosition];
+      total.style.color=colors[colorPosition];
       console.log(totalTodo)
   },[totalTodo])
 
@@ -58,18 +63,16 @@ export default function App() {
   return (
     <div className="app">
     <h2>Mi lista de la compra🛒</h2>
-
       <input type="text" id="input-todo"/>
-
       <button 
       onClick={() => {addTodo(document.getElementById("input-todo").value)}}>
       Añadir artículo</button>
       <p >Has añadido <span id="total">{totalTodo}</span> artículos</p>
+
       <ul>
       {todos.map(todo =>(
         <div className="todoContainer">
           <li className="todoItem" key={todo}>{todo}</li>
-          {/* key={todo} */}
           <button className="todoButton" onClick={() => deleteTodo(todo)}>x</button>          
         </div>
         ))}
